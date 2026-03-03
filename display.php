56b8fd2130036bf4f2850ecf441f4fc191878f54
Rita <umezinwarita464@gmail.com>
2026-03-03 17:30:03 +0100

    fix: update form action in biodata.html and add display.php for form handling

---
+++ display.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST['submit']))
+$fullname = $_POST['fullname'];
+$date = $_POST['dob'];
+$gender = $_POST['gender'];
+$nationality = $_POST['nationality'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$service= $_POST['service'];
+
+?>
