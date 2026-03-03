7518abaa4401c4d4f02c304e56db7c8567cbf03d
Rita <umezinwarita464@gmail.com>
2026-03-03 17:38:06 +0100

    fix: remove redundant PHP code and clean up display.php

--- display.php
+++ display.php
@@ -20,17 +20,4 @@ echo "Address: " . $address . "<br>";
 echo "Service: " . $service . "<br>";
 
 ?>
-?>
-<?php
 
-if(isset($_POST['submit']))
-$fullname = $_POST['fullname'];
-$date = $_POST['dob'];
-$gender = $_POST['gender'];
-$nationality = $_POST['nationality'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$address = $_POST['address'];
-$service= $_POST['service'];
-
-?>
